c1344ac00e09a75ac75525abbc1ce867b1370070
warleyalmeida <warleyalmeida@brq.com>
2025-06-22 20:09:44 -0300

    2626. Array Reduce Transformation

---
+++ ArrayReduceTransformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res = init
+
+    nums.forEach(function(num, i){
+        res = fn(res, num);
+    })
+
+    return res
+};
