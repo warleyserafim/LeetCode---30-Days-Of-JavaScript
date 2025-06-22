ac08365a8a475414f646785d5e0905f9b27386d5
warleyalmeida <warleyalmeida@brq.com>
2025-06-22 20:06:43 -0300

    2665. Counter II

---
+++ CounterII.js
@@ -0,0 +1,30 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+
+    let initialValue = init;
+
+    function increment() {
+        return ++init;
+    };
+
+    function reset() {
+        return init = initialValue;
+    };
+
+    function decrement() {
+        return --init;
+    }
+
+    return {increment, reset, decrement};
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
